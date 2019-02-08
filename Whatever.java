392563d6077197b91a25e80e8488a258ee11ef76
tojejl <45316403+tojejl@users.noreply.github.com>
2019-02-08 11:58:11 -0600

    Add files via upload

---
+++ Whatever.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Whatever {
+
+    public static void theFunctionName()
+    {
+        //do stuff here
+        System.out.println("With a knick-knack paddywhack,");
+        System.out.println("Give the dog a bone,");
+        System.out.println("This old man came rolling home.");
+        System.out.print("\n");
+    }
+
+
+    public static void main(String[] args)
+    {
+	// call a function
+        System.out.println("This old man, he played one.");
+        System.out.println("He played knick-knack on my thumb;");
+        theFunctionName();
+        System.out.println("This old man, he played two.");
+        System.out.println("He played knick-knack on my shoe;");
+        theFunctionName();
+        System.out.println("This old man, he played three.");
+        System.out.println("He played knick-knack on my knee;");
+        theFunctionName();
+        System.out.println("This old man, he played four.");
+        System.out.println("He played knick-knack on my door;");
+        theFunctionName();
+        System.out.println("This old man, he played five.");
+        System.out.println("He played knick-knack on my hive;");
+        theFunctionName();
+        System.out.println("This old man, he played six.");
+        System.out.println("He played knick-knack on my sticks;");
+        theFunctionName();
+    }
+}
