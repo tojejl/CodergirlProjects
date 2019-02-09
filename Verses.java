304472033b4c53d190541c65988ef08e9e5cfffe
tojejl <45316403+tojejl@users.noreply.github.com>
2019-02-09 16:00:29 -0600

    Add files via upload

---
+++ Verses.java
@@ -0,0 +1,67 @@
+package com.company;
+
+public class Verses {
+
+    public static void thisOldMan1()
+    {
+        //do stuff here
+        System.out.println("This old man, he played one.");
+        System.out.println("He played knick-knack on my thumb.");
+
+    }
+    public static void thisOldMan2()
+    {
+        System.out.println("This old man, he played two.");
+        System.out.println("He played knick-knack on my shoe.");
+    }
+
+    public static void thisOldMan3()
+    {
+        System.out.println("This old man, he played three.");
+        System.out.println("He played knick-knack on my knee.");
+    }
+
+    public static void thisOldMan4()
+    {
+        System.out.println("This old man, he played four.");
+        System.out.println("He played knick-knack on my door.");
+    }
+
+    public static void thisOldMan5()
+    {
+        System.out.println("This old man, he played five.");
+        System.out.println("He played knick-knack on my hive.");
+    }
+
+    public static void thisOldMan6()
+    {
+        System.out.println("This old man, he played six.");
+        System.out.println("He played knick-knack on my sticks.");
+    }
+
+    public static void knickKnack()
+    {
+        System.out.println("With a knick-knack paddywhack,");
+        System.out.println("Give the dog a bone,");
+        System.out.println("This old man came rolling home.");
+        System.out.print("\n");
+    }
+
+
+    public static void main(String[] args)
+    {
+	// call a function
+        thisOldMan1();
+        knickKnack();
+        thisOldMan2();
+        knickKnack();
+        thisOldMan3();
+        knickKnack();
+        thisOldMan4();
+        knickKnack();
+        thisOldMan5();
+        knickKnack();
+        thisOldMan6();
+        knickKnack();
+    }
+}
